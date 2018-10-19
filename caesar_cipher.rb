711bf298becfdf7c1c9b719b82e7d7b36382fe62
paramagician <konnor5456@gmail.com>
2018-10-19 22:23:57 +0000

    lots of progress & tests

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,5 +1,31 @@
 # frozen_string_literal: true
 
+# Encrypts & Decrypts plain text
 class CaesarCipher
-  ALPHABET = [*('a'..'z'), *('A'..'Z')].freeze
+  def initialize
+    @alphabet = [*('a'..'z'), *('A'..'Z')].join
+  end
+
+  def check_args(shift:, text:)
+    raise ArgumentError unless shift.is_a? Integer
+    raise ArgumentError unless text.is_a? String
+  end
+
+  def encrypt(shift:, text:)
+    check_args(shift: shift, text: text)
+    text.tr(make_cipher, @alphabet)
+  end
+
+  def decrypt(shift:, text:)
+    check_args(shift: shift, text: text)
+    text.tr(@alphabet, make_cipher)
+  end
+
+  private
+
+  def make_cipher
+    # duplicate alphabet, create array of chars, rotate(n) times
+    # join to make a string
+    @alphabet.dup.chars.rotate(shift).join
+  end
 end
--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -7,17 +7,43 @@ RSpec.describe CaesarCipher do
 
   context 'creates a full alphabet' do
     it 'check that it has lower case' do
-      expect(CaesarCipher::ALPHABET[25]).to eq 'z'
+      expect(CaesarCipher.alphabet[25]).to eq 'z'
     end
 
     it 'check that it has uppercase' do
-      expect(CaesarCipher::ALPHABET[-1]).to eq 'Z'
+      expect(CaesarCipher.alphabet[-1]).to eq 'Z'
     end
   end
 
   context '#initialize' do
     it 'creates a caesar_cipher object' do
       expect(cc).to be_an_instance_of CaesarCipher
+    end
+  end
+
+  context 'encrypt(shift, text)' do
+    it 'wraps around' do
+      expect cc.encrypt(shift: 2, text: 'z x ab').to eq 'b a ce'
+    end
+
+    it 'accounts for  > 26' do
+      expect cc.encrypt(shift: 27, text: 'abc').to eq 'cde'
+    end
+
+    it 'accounts for negative numbers' do
+      expect cc.encrypt(shift: -2, text: 'ab').to eq 'xy'
+    end
+
+    it 'accounts for uppercase letters' do
+      expect cc.encrypt(shift: 3, text: 'Z X A b c'). to eq 'C B D e f'
+    end
+
+    it 'raises an argument error if shift is not an integer' do
+      expect { cc.encrypt(shift: 'h', text: 'y') }.to raise_error ArgumentError
+    end
+
+    it 'raises an ArgumentError if text is not a string' do
+      expect { cc.encrypt(shift: 2, text: not_string) }.to raise_error ArgumentError
     end
   end
 end
